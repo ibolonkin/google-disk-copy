6a26447b89c6f38a8d47270ae1cb1eff456528c7
ibolonkin <ivanbolonkin111@gmail.com>
2024-08-15 18:37:09 +0500

    add validation on Sign Up Form

--- frontend/src/components/Registration/Registration.js
+++ frontend/src/components/Registration/Registration.js
@@ -1,10 +1,54 @@
+import { useEffect, useState } from 'react';
 import './style.css';
 
 const Registration = () => {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const [isDisabled, setIsDisabled] = useState(false);
+
+    function isValidEmail(email){
+        return /^\S+@\S+\.\S+$/.test(email);
+    }
+
+    const handleEmailChange = event => {
+
+        const newEmail = event.target.value;
+        setEmail(newEmail);
+        
+        if(!isValidEmail(event.target.value)){
+            setError('Invalid Email');
+            setIsDisabled(true);
+        } else {
+            setError(null);
+        }
+        
+        updateButtonState();
+    }
+
+    const handleNameChange = event => {
+        setName(event.target.value);
+        updateButtonState();
+    }
+
+    const handlePasswordChange = event => {
+        setPassword(event.target.value);
+        updateButtonState();
+    }
+
+    const updateButtonState = () => {
+        const isFormValid = isValidEmail(email) && name !== '' && password !== '';
+        setIsDisabled(!isFormValid);
+    }
+
+    useEffect(() => {
+        updateButtonState();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [email, name, password]);
+
     return (
         <>
-            
-
             <div className='Registration'>
                 <form className='form'>
                     <h1 className='title'>Sign Up</h1>
@@ -15,13 +59,23 @@ const Registration = () => {
                             type='text' 
                             placeholder='Name' 
                             maxlength="16"
+                            value={name}
+                            onChange={handleNameChange}
                         />
                     </div>
 
                     <div className='inputContainer'>
                         
-                        <input className='input' type='email' placeholder='Email' />
+                        <input 
+                            className='input'
+                            type='email'
+                            placeholder='Email'
+                            value={email}
+                            onChange={handleEmailChange}
+                        />
                     </div>
+                    {error && <p style={{color: 'red'}}>{error}</p>}
+
 
                     <div className='inputContainer'>
                         
@@ -33,9 +87,11 @@ const Registration = () => {
                             minlength="4"
                             maxlength="12"
                             size="16"
+                            value={password}
+                            onChange={handlePasswordChange}
                         />
                     </div>
-                    <input className='registrationBtn' type="submit" value="Sign Up" />
+                    <input className='registrationBtn' disabled={isDisabled} type="submit" value="Sign Up" />
                 </form>
             </div>
         </>
